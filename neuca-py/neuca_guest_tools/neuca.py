cfeff2750e283ff424367ab470e86f72826be954
paul-ruth <paul.ruth@gmail.com>
2015-08-12 14:12:08 -0400

    update

--- neuca-py/neuca_guest_tools/neuca.py
+++ neuca-py/neuca_guest_tools/neuca.py
@@ -447,7 +447,7 @@ class NEucaCometData(NEucaData):
         ifaces_rtn=[]
         for iface in ifaces:
             print "iface: " + str(iface)
-            ifaces_rtn.append=[mac, state + ":" + ip_type + ":" + ip ]
+            ifaces_rtn.append=[mac, ifaces[mac]['state'] + ":" + ifaces[mac]['protocol'] + ":" + ifaces[mac]['ip_address'] ]
         #
         #print "RETURNING hostname = " + hostname
 
